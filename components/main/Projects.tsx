104bf388db9d5f58d256acf1fe6ccf763bb4a91a
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-04-19 15:18:59 +0530

    Add files via upload

---
+++ components/main/Projects.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/projectt11.png"
+          title="Codeial"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/projectt22.png"
+          title="HooBank"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+       
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
